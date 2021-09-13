c0ea6da60ae807135edc6ced3011931ddd17af72
tomgrice <tomgrice@gmail.com>
2021-09-13 04:35:52 +0100

    lambda update

--- Lambda/CloudRig-DeleteVolumeOldSnapshots/lambda_function.py
+++ Lambda/CloudRig-DeleteVolumeOldSnapshots/lambda_function.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     SNAPSHOT_ID = event['detail']['snapshot_id'].split('/')[1]
     
     #SNAPSHOT_REGION = 'eu-west-2'
-    #SNAPSHOT_ID = 'snap-098ea2061bdf2dc6c'
+    #SNAPSHOT_ID = 'snap-00934cec8fdc15ec2'
     
     ec2 = boto3.client('ec2',region_name=SNAPSHOT_REGION)
     res = boto3.resource('ec2',region_name=SNAPSHOT_REGION)
@@ -27,6 +27,18 @@ def lambda_handler(event, context):
         return tag_value
         
         
+    def deregister_image(snapid):
+        images = ec2.describe_images(Filters=[{'Name': 'name', 'Values':[snapid]}])['Images']
+        
+        result = []
+        for image in images:
+            imageid = image['ImageId']
+            image_res = res.Image(imageid)
+            dereg_image = image_res.deregister(DryRun=DryRunSetting)
+            result.append(dereg_image)
+                
+        return result
+        
     def delete_snapshots(name,immune_snapshotid):
         snapshots = ec2.describe_snapshots(Filters=[{'Name': 'tag:Name', 'Values':[name]}])['Snapshots']
         
@@ -40,6 +52,29 @@ def lambda_handler(event, context):
                 result.append(delete_snap)
                 
         return result
+        
+    def create_image(snapname, snapid):
+        ami = ec2.register_image(
+            Name=snapname, 
+            Description=snapname + ' Automatic AMI', 
+            BlockDeviceMappings=[
+                {
+                    'DeviceName': '/dev/sda1',
+                    'Ebs': {
+                        'DeleteOnTermination': False,
+                        'SnapshotId': snapid,
+                        'VolumeType': 'gp2'
+                    }
+                },
+            ],
+            Architecture='x86_64', 
+            RootDeviceName='/dev/sda1', 
+            DryRun=False, 
+            VirtualizationType='hvm', 
+            EnaSupport=True
+        )
+        
+        return ami
 
     snapshot = res.Snapshot(SNAPSHOT_ID)
     
@@ -52,6 +87,7 @@ def lambda_handler(event, context):
         if snapshot_name != '':
             volume = res.Volume(snapshot.volume_id)
             volume.delete(DryRun=DryRunSetting)
-            
-            delete_snapshots(snapshot_name,SNAPSHOT_ID)
+            print(deregister_image(snapshot_name))
+            print(delete_snapshots(snapshot_name,SNAPSHOT_ID))
+            print(create_image(snapshot_name, SNAPSHOT_ID))
             
