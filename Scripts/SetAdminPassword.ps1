948924c83cc91e5246c82018df2041014bf8b199
tomgrice <tomgrice@gmail.com>
2021-10-03 03:10:44 +0100

    Update SetAdminPassword.ps1

--- Scripts/SetAdminPassword.ps1
+++ Scripts/SetAdminPassword.ps1
@@ -11,19 +11,12 @@ function Disable-CAD {
     Write-Host "Ctrl-Alt-Del to log in has been disabled." -ForegroundColor Green
 }
 
-function AutoLogin([string]$User,[string]$Pass) {
-    $AutoLoginPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-    Set-ItemProperty $AutoLoginPath 'AutoAdminLogon' -Value "1" -Type String
-    Set-ItemProperty $AutoLoginPath 'DefaultUsername' -Value "$User" -Type String
-    Set-ItemProperty $AutoLoginPath 'DefaultPassword' -Value "$Pass" -Type String
-    Write-Host "Auto-login has been enabled." -ForegroundColor Green
-}
-
 $JSON = ConvertFrom-Json (Get-Content "C:\ProgramData\Amazon\EC2-Windows\Launch\Config\LaunchConfig.json" -raw)
 $JSON.adminPasswordType = "DoNothing"
 $JSON | ConvertTo-Json | Set-Content "C:\ProgramData\Amazon\EC2-Windows\Launch\Config\LaunchConfig.json"
 
 Disable-CAD
-AutoLogin "$AdminUser" "$AdminPassword"
+choco install autologon -y
+Start-Process AutoLogon -ArgumentList "/accepteula", $AdminUser, $env:UserDomain, $AdminPassword -Wait
 
 Remove-Item "C:\imageconfig.json" -Force
