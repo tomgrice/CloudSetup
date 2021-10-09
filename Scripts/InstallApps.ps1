83fd6966f7c1ca7a02891a024d4c8a463df2075b
Tom Grice <tomgrice@gmail.com>
2021-10-09 12:14:08 +0000

    t

--- Scripts/InstallApps.ps1
+++ Scripts/InstallApps.ps1
@@ -1,4 +1,11 @@
 foreach ($AppName in $ScriptConfig.ChocoInstall)
 {
-    Start-Process choco -ArgumentList "install", $AppName, "-y"
+    Start-Process choco -ArgumentList "install", $AppName, "-y" -NoNewWindow -Wait
 }
+
+"https://d1uj6qtbmh3dt5.cloudfront.net/" | Set-Variable BucketURL -Scope Private ; Set-Variable DCVUrl -Value ("$BucketURL" + ((Invoke-RestMethod "$BucketURL").ListBucketResult.Contents | Where-Object {$_.Key -like "*/Servers/*.msi"} | Sort-Object {$_.LastModified} -Descending | Select-Object -First 1).Key)
+Write-Host "Installing NICE-DCV from $DCVUrl"
+Invoke-RestMethod $DCVUrl -OutFile "$InstallDir\NiceDCV.msi"
+
+Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i $InstallDir\NiceDCV.msi ADDLOCAL=ALL AUTOMATIC_SESSION_OWNER=Administrator /quiet /norestart" -Wait
+New-Item -Path "Microsoft.PowerShell.Core\Registry::\HKEY_USERS\S-1-5-18\Software\GSettings\com\nicesoftware\dcv\" -Name security -Force | Set-ItemProperty -Name os-auto-lock -Value 0
--- Scripts/PreSetup.ps1
+++ Scripts/PreSetup.ps1
@@ -13,13 +13,6 @@ Invoke-RestMethod -Uri 'https://api.github.com/repos/tomgrice/CloudSetup/zipball
 Expand-Archive -Path "C:\CloudSetup.zip" -DestinationPath "C:\"
 Move-Item -Path "C:\*CloudSetup*" -Destination $InstallDir
 
-"https://d1uj6qtbmh3dt5.cloudfront.net/" | Set-Variable BucketURL -Scope Private ; Set-Variable DCVUrl -Value ("$BucketURL" + ((Invoke-RestMethod "$BucketURL").ListBucketResult.Contents | Where-Object {$_.Key -like "*/Servers/*.msi"} | Sort-Object {$_.LastModified} -Descending | Select-Object -First 1).Key)
-Write-Host "Installing NICE-DCV from $DCVUrl"
-Invoke-RestMethod $DCVUrl -OutFile "$InstallDir\NiceDCV.msi"
-
-Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList "/i $InstallDir\NiceDCV.msi ADDLOCAL=ALL AUTOMATIC_SESSION_OWNER=Administrator /quiet /norestart" -Wait
-New-Item -Path "Microsoft.PowerShell.Core\Registry::\HKEY_USERS\S-1-5-18\Software\GSettings\com\nicesoftware\dcv\" -Name security -Force | Set-ItemProperty -Name os-auto-lock -Value 0
-
 #Disable Password Complexity
 secedit /export /cfg c:\secpol.cfg
 (Get-Content C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
